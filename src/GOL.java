c7dbb95c189f3b26e3894b1afe2bce9d97034459
chy <1010124060@qq.com>
2023-05-24 17:21:00 +0800

    生命游戏

--- src/GOL.java
+++ src/GOL.java
@@ -6,31 +6,31 @@ public class GOL {
     public static final int hNUM = 30;
     int width = SIZE*wNUM ;
     int height = SIZE*hNUM;
+    private boolean[][] ar = new boolean[hNUM][wNUM];
 
 
     private boolean [][]array = new boolean[hNUM][wNUM];
     GOL()
     {
-        for(int i=0;i<wNUM;i++)
-        {
-            for(int j=0;j<hNUM;j++)
-            {
-                array[i][j]=false;
-            }
-        }
+
     }
-    private int getNeighborNum(int x,int y)
+    public boolean getArray(int x,int y)
+    {
+        return array[x][y];
+    }
+    public void SetArray(int x,int y)
+    {
+        array[x][y] = !array[x][y];
+    }
+    public int getNeighborNum(int x,int y)
     {
         int num=0;
-        //if(x==0 || y==0){
-        //
-        //}
         for(int i=x-1;i<=x+1;i++)
         {
             for(int j=y-1;j<=y+1;j++)
             {
-                if(i<0 || i>=wNUM || j<0 || j>=hNUM) continue;
-                if(i==x && j==y) continue;
+                if(i<0||i>=wNUM||j<0||j>=hNUM) continue;
+                if(i==x&&j==y) continue;
                 if(array[i][j]==true) num++;
             }
         }
@@ -39,26 +39,36 @@ public class GOL {
 
     public void getNextState()
     {
+
         for(int i=0;i<wNUM;i++)
         {
+
             for(int j=0;j<hNUM;j++)
             {
                 if(getNeighborNum(i,j)==3)
                 {
-                    array[i][j]=true;
+                    ar[i][j]=true;
                 }
                 else if(getNeighborNum(i,j)==2)
                 {
-                    continue;
+
+                    ar[i][j] = array[i][j];
                 }
                 else
                 {
-                    array[i][j]=false;
+                    ar[i][j]=false;
                 }
             }
-        }System.out.println(111);
+        }
+        for(int i=0;i<wNUM;i++)
+        {
+            for(int j=0;j<hNUM;j++)
+            {
+                array[i][j] = ar[i][j];
+            }
+        }
     }
-    public void randomCreate()
+    public void getExample()
     {
         Random r = new Random();
         int num = r.nextInt(wNUM*hNUM/4);
--- src/WindowGOL.java
+++ src/WindowGOL.java
@@ -1,7 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
-import java.util.TimerTask;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.Component;
+
 
 public class WindowGOL extends JFrame {
     private GOL gol=new GOL();
@@ -9,78 +11,138 @@ public class WindowGOL extends JFrame {
     private JButton Introduction=new JButton();
     private JButton bRandom=new JButton();
     private JButton Clear=new JButton();
-    private JPanel jPanel=new JPanel();
+    private JPanel bottomPanel =new JPanel();
+
+    private JPanel backPanel = new JPanel(new BorderLayout());
+    private JPanel centerPanel =new JPanel(new GridLayout(GOL.wNUM, GOL.hNUM));
+
+
     private GridLayout gridlayout = new GridLayout(GOL.wNUM, GOL.hNUM);
-    private Timer timer=new Timer(1000, new AbstractAction() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            gol.getNextState();
-        }
-    });
-    WindowGOL(){
+    private  JButton[][] TWorld;
+    WindowGOL()
+    {
+        initWindowGOL();
+    }
+
+    public void initWindowGOL(){
         //窗口属性设置
         this.setSize(GOL.SIZE*GOL.wNUM,GOL.SIZE*GOL.hNUM);
         this.setVisible(true);
         this.setLocationRelativeTo(null);
         this.setTitle("生命游戏");
-        this.setDefaultCloseOperation(3);
+        //this.setDefaultCloseOperation(3);
         //jPanel容器设置
-        this.add(jPanel);
-        jPanel.setVisible(true);
+        this.setContentPane(backPanel);
+        backPanel.add(centerPanel,"Center");
+        backPanel.add(bottomPanel,"South");
+        //bottomPanel.setVisible(true);
+        //this.pack();
+        //jPanel.setLayout(null);
+        int sizeLx,sizeLy;
+        sizeLx=(GOL.wNUM)* GOL.SIZE;
+        sizeLy=(GOL.hNUM)* GOL.SIZE;
+        this.setSize(sizeLx,sizeLy);
+        TWorld=new JButton[GOL.wNUM][GOL.hNUM];//二维按钮数组
+        //添加网格
+        class MyMouseListener extends MouseAdapter {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                JButton buttonClicked = (JButton) e.getSource();
+                buttonClicked.setBackground(Color.RED);
+                //buttonClicked.
+                //buttonClicked.getLocation().
+                int x= (buttonClicked.getLocation().x-8)/ (GOL.SIZE-1);
+                int y= (buttonClicked.getLocation().y-7)/ (GOL.SIZE-3);
+                System.out.println(buttonClicked.getLocation().x);
+                System.out.println(buttonClicked.getLocation().y);
+                System.out.println(centerPanel.getLocation().x);
+                System.out.println(centerPanel.getLocation().y);
+                System.out.println(x+" "+y);
+                gol.SetArray(y,x);
+                showWorld();
+
+            }
+        }
+        //jPanel.add()
+        for (int i = 0; i < GOL.wNUM; i++) {
+            for (int j = 0; j < GOL.hNUM; j++) {
+                TWorld[i][j]=new JButton("");
+                TWorld[i][j].setBackground(Color.white);
+                TWorld[i][j].addMouseListener(new MyMouseListener());
+                centerPanel.add(TWorld[i][j]);
+            }
+        }
         //StartAndEnd按钮设置
-        jPanel.add(SE);
+        bottomPanel.add(SE);
+
         SE.setText("Start");
+        //SE.setBounds(600,600,10,10);
         SE.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                if(SE.getText()==("Start")){
-                    SE.setText("End");
-                    SE.repaint();
-                    timer.start();
+                if(SE.getText()==("Start"))
+                {
+                    //SE.setText("End");
+                    showWorld();
+                    changeState();
+
                 }
-                if(SE.getText()==("End")){
+                else if(SE.getText()=="End")
+                {
                     SE.setText("Start");
-                    SE.repaint();
-                    timer.stop();
                 }
+                SE.repaint();
             }
         });
         //介绍按钮设置
-        jPanel.add(Introduction);
+        bottomPanel.add(Introduction);
         Introduction.setText("Introduction");
-        Introduction.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                String introductionMessage="<html>本程序由软件zy2101班赵佳鹏和陈宏扬设计完成<br>" +
-                        "这是0人游戏，即按照初始的设置，游戏自动演化。<br>" +
-                        "在类似围棋的棋盘中，每一个格子可以是空格或者存在一个生命/细胞/Cell；<br>" +
-                        "每一个格子有8个相邻的格子，相邻的格子中存活的生命数量称为其邻居(neighbor)数。<br>" +
-                        "在世代交替时，所有的格子根据其邻居数，诞生新生命、Cell保持存活或者Cell死亡。<br>" +
-                        "生命演化规则：B3/S23<br>" +
-                        "一个生命如果恰好有2个或3个邻居，它会存活到下一个世代；否则，会因为孤独或拥挤而死亡。<br>" +
-                        "一个空格，如果恰好有3个邻居，则诞生一个新生命。<html>";
-                JDialog jDialog=new JDialog();
-                jDialog.setTitle("介绍");
-                JLabel jLabel=new JLabel(introductionMessage);
-                jDialog.add(jLabel);
-                jDialog.setSize(480,260);
-                jDialog.setLocationRelativeTo(null);
-                jDialog.setVisible(true);
-            }
-        });
         //清除按钮设置
-        jPanel.add(Clear);
+        bottomPanel.add(Clear);
         Clear.setText("Clear");
         Clear.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 gol.clear();
+                showWorld();
             }
         });
         //随机生成按钮设置
-        jPanel.add(bRandom);
+        bottomPanel.add(bRandom);
         bRandom.setText("Random");
-        //添加网格
-        //jPanel.add()
+        bRandom.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                gol.getExample();
+                showWorld();
+            }
+        });
+
+
+        //this.pack();
+
+
+
     }
+    public void changeState()
+    {
+        //System.out.println("1111");
+        gol.getNextState();
+        //System.out.println("2222");
+        showWorld();
+    }
+    //二维数组可视化显示
+    public void showWorld(){
+        for(int i=0;i<GOL.wNUM;i++){
+            for(int j=0;j<GOL.hNUM;j++){
+                if(gol.getArray(i,j)){
+                    TWorld[i][j].setBackground(Color.red);
+                }
+                else{
+                    TWorld[i][j].setBackground(Color.white);
+                }
+            }
+        }
+    }
+
 }
